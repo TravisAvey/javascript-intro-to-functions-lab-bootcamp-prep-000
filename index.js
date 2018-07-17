c1d7b071118ef80d5331a55b98112dcdf2184e7b
Travis Avey <avey.travis@gmail.com>
2018-07-17 20:05:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ function logShout(string) {
   console.log(string.toUpperCase())
 }
 
-function logWhisper(string) }
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var s = string
+  if (s.toLowerCase() === string) {
+    return "I can't hear you"
+  } else if (s.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else if (s == "I love you, Grandma.") {
+    return "I love you, too."
+  }
+}
